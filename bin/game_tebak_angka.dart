247841bff986a816a2ade9423316f24f8b0b2e17
gerly <gherly.ylrehg@gmail.com>
2025-09-17 19:44:07 +0700

    Tambah fungsi cek bilangan prima dengan perulangan for-in

--- bin/game_tebak_angka.dart
+++ bin/game_tebak_angka.dart
@@ -9,6 +9,14 @@ int hitungFaktorial(int n) {
   return hasil;
 }
 
+bool cekPrima(int n) {
+  if (n < 2) return false;
+  for (int i = 2; i <= n ~/ 2; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+
 void main() {
   print("=======================================");
   print("          MENU PROGRAM DART             ");
