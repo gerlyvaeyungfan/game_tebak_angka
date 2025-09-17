1e7992af761a9fd1cb6ed0343ed1b455ee2375e7
gerly <gherly.ylrehg@gmail.com>
2025-09-17 19:18:08 +0700

    first commit

---
+++ bin/game_tebak_angka.dart
@@ -0,0 +1,5 @@
+import 'package:game_tebak_angka/game_tebak_angka.dart' as game_tebak_angka;
+
+void main(List<String> arguments) {
+  print('Hello world: ${game_tebak_angka.calculate()}!');
+}
---
+++ test/game_tebak_angka_test.dart
@@ -0,0 +1,8 @@
+import 'package:game_tebak_angka/game_tebak_angka.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
